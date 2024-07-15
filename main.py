677566290114dd055e97cc4d61cf68dcc9527d5b
Евгений Александрович Волчёк <zehrbit@gmail.com>
2024-07-15 08:37:54 +0300

    Добавил возможность регистрироваться

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from models import User
-from schemas import UserCreate
 from database import engine, Base, get_db
 
 Base.metadata.create_all(bind=engine)
